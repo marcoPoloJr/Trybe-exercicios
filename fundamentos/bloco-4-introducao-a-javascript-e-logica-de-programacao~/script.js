9a9597445e803f499ace46bba4b8b053b31667a6
Marco Polo <seguros.marcopolo@gmail.com>
2022-07-20 18:48:49 -0300

    Operação multiplicação, divisão e módulo realizados

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/script.js
@@ -6,4 +6,12 @@ console.log(soma)
 //subtração
 const subtração =(a - b)
 console.log(subtração)
-
+//multiplicação
+const multiplicação = (a * b)
+console.log(multiplicação)
+//divisão
+const divisão = (a/b)
+console.log(divisão)
+//modulo
+const modulo = (a % b)
+console.log(modulo)
