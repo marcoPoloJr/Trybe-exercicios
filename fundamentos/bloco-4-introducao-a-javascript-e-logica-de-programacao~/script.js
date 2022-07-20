9c0df1f9e96b35243d70f516d503272fb44ed4fd
Marco Polo <seguros.marcopolo@gmail.com>
2022-07-20 18:58:31 -0300

    Programa que retorne o maior numero realizado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/script.js
@@ -1,17 +1,8 @@
-//soma
 const a =10
-const b =10
-const soma = (a + b)
-console.log(soma)
-//subtração
-const subtração =(a - b)
-console.log(subtração)
-//multiplicação
-const multiplicação = (a * b)
-console.log(multiplicação)
-//divisão
-const divisão = (a/b)
-console.log(divisão)
-//modulo
-const modulo = (a % b)
-console.log(modulo)
+const b = 15
+if (a > b){
+    console.log('a é maior que b')
+}
+else {
+    console.log('b é maior que a')
+    }
