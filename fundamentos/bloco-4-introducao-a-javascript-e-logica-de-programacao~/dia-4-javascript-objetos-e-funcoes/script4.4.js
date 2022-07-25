520b2aa55801bc79acd99f129d33cb8c0c1c18f7
Marco Polo <seguros.marcopolo@gmail.com>
2022-07-25 19:13:02 -0300

    Exercicio 2 realizado.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/dia-4-javascript-objetos-e-funcoes/script4.4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/dia-4-javascript-objetos-e-funcoes/script4.4.js
@@ -2,7 +2,12 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
   };
   //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
       console.log('Bem-vinda, '+info.personagem)    
+      console.log(info)
   
+//🚀 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
+
+    console.log(info)
