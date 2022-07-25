8adebe0f78788acb2e51db950fe2fac3aa40c95f
Marco Polo <seguros.marcopolo@gmail.com>
2022-07-25 19:37:24 -0300

    Exercicio 5 realizado.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/dia-4-javascript-objetos-e-funcoes/script4.4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/dia-4-javascript-objetos-e-funcoes/script4.4.js
@@ -24,3 +24,18 @@ for(let key in info){
 for(let key in info){
     console.log(info[key])
 }
+
+//5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 
+//'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto 
+//juntos de acordo com cada uma das chaves.
+
+let info2={
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+}
+console.log(info.personagem + ' e ' + info2.personagem)
+console.log(info.origem + ' e ' + info2.origem)
+console.log(info.nota + ' e ' + info2.nota)
+console.log('Ambos recorrentes')
