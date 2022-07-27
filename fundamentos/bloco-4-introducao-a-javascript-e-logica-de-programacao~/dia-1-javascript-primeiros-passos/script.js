2953fd0e1da615174d4f042d548a4c71a322cb16
Marco Polo <seguros.marcopolo@gmail.com>
2022-07-27 18:16:26 -0300

    finalização de exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,11 @@
+const pecaXadrez = 'rainha'
+
+switch (pecaXadrez.toLowerCase()){
+    case 'rei':
+        console.log('Rei-> Uma casa apenas para qualquer direção.')
+break;
+    case 'bispo'
+        console.log(move para todos os lados)
+break;
+
+    }
